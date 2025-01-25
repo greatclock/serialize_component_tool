ea8099a35d4b667cc8fba88ba241ebf80061a7a1
greatclock <thegreatclock@gmail.com>
2025-01-25 22:01:29 +0800

    If `public void Clear()` exists in supported component type, it will be invoked in 'Clear' method of generated script.

--- Editor/SerializedComponentClasses.cs
+++ Editor/SerializedComponentClasses.cs
@@ -36,7 +36,15 @@ namespace GreatClock.Common.SerializeTools {
 				s_temp_strings.Clear();
 				if (requireClearOnRecycle) {
 					BindingFlags flags = BindingFlags.Instance | BindingFlags.Public;
+					bool hasClear = false;
 					foreach (MemberInfo member in type.GetMembers(flags)) {
+						MethodInfo mi = member as MethodInfo;
+						if (mi != null) {
+							if (mi.Name == "Clear" && mi.GetParameters().Length <= 0 && mi.ReturnType == s_type_void) {
+								hasClear = true;
+							}
+							continue;
+						}
 						Type fieldType = null;
 						PropertyInfo pi = member as PropertyInfo;
 						FieldInfo fi = member as FieldInfo;
@@ -44,9 +52,10 @@ namespace GreatClock.Common.SerializeTools {
 						if (fi != null) { fieldType = fi.FieldType; }
 						if (fieldType == null || !fieldType.IsSubclassOf(s_type_event_base)) { continue; }
 						if (member.GetCustomAttribute<ObsoleteAttribute>() != null) { continue; }
-						s_temp_strings.Add($"{member.Name}.RemoveAllListeners()");
+						s_temp_strings.Add(string.Format("{0}.RemoveAllListeners()", member.Name));
 					}
 					s_temp_strings.Sort();
+					if (hasClear) { s_temp_strings.Add("Clear()"); }
 				}
 				mClearCalls = s_temp_strings.ToArray();
 				s_temp_strings.Clear();
@@ -54,6 +63,7 @@ namespace GreatClock.Common.SerializeTools {
 			return mClearCalls;
 		}
 
+		private static Type s_type_void = typeof(void);
 		private static Type s_type_event_base = typeof(UnityEventBase);
 		private static List<string> s_temp_strings = new List<string>();
 
